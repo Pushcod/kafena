93169aa38485177289fa357543023bb0c080c6b6
Pushcod <4programart1@gmail.com>
2024-02-02 17:12:37 +0300

    добавили  брейкпоинты для адапатации верстки сайта

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    screens: {
+      '2xl': {'max': '1400px'},
+      // => max-width: 1535px { ... }
+      
+      'xl': {'max': '1200px'},
+      // => max-width: 1279px { ... }
+      
+      'lg': {'max': '1024px'},
+      // => max-width: 1023px { ... }
+      
+      'md': {'max': '768px'},
+      // => max-width: 767px { ... }
+      
+      'sm': {'max': '400px'},
+      // => max-width: 639px { ... }
+      }
+  },
+  plugins: [],
+};
